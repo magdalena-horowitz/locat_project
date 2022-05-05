792d168281b7225ea63b810b0b03caad766ef39b
Mayur Bhandary <mayurbhandary96@gmail.com>
2022-05-05 19:10:45 -0400

    makes discounts component

--- lib/screens/discount/discount_screen.dart
+++ lib/screens/discount/discount_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/models/Cart.dart';
+
 import 'components/body.dart';
 import 'components/check_out_card.dart';
 
-class CartScreen extends StatelessWidget {
+class DiscountScreen extends StatelessWidget {
   static String routeName = "/cart";
   @override
   Widget build(BuildContext context) {
